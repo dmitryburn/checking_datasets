486ecb7ccdc9270fc331f851a10fee8719b36023
dmitryburn <bunya68@gmail.com>
2024-09-10 16:23:29 +0300

    add new datates and fix class trandformer

--- src/dataset_transformer.py
+++ src/dataset_transformer.py
@@ -1,93 +1,62 @@
-import pandas as pd
-import re
 from sklearn.impute import SimpleImputer
 from sklearn.model_selection import train_test_split
-from imblearn.over_sampling import SMOTE
 
 class BaseDatasetTransform:
-    def __init__(self, dataset, target = None,dataset_test=None):
+    def __init__(self, dataset, target=None, dataset_test=None):
         self.dataset = dataset
-
         if isinstance(target, str):
-            self.target = [target]  
+            self.target = [target]
         elif isinstance(target, list):
-            self.target = target   
+            self.target = target
         else:
             raise ValueError("Target should be string or a list of strings")
-
         self.dataset_test = dataset_test
 
     def _remove_id_columns(self, X):
-
         cols = X.columns
-
-
-        #id_columns = [col for col in cols if col.lower() == 'id']
         id_columns = [col for col in cols if 'id' in col.lower()]
-
         if id_columns:
             print(f'\nОбнаружены колонки с именем "id": {id_columns}')
-        
             for col in id_columns:
                 response = input(f'Хотите удалить колонку "{col}"? (y/n): ').strip().lower()
                 if response == 'y':
                     X = X.drop(columns=[col])
                     print(f'Колонка "{col}" удалена.')
-
         return X
 
-    def fit_transform(self):
-
-        self.dataset = self._remove_id_columns(self.dataset)
-        if self.target:
-            #if self.target in self.dataset.columns:
-                #target_missing = self.dataset[self.target].isna().sum()
-                target_missing = self.dataset[self.target].isna().sum(axis=0)
-
-                target_missing_columns = target_missing[target_missing>0]
-                print(target_missing_columns)
-                if not target_missing_columns.empty:
-                    for column in target_missing_columns:
-                        print(f'\nВ колонке {column} есть пропущенные значения:')
-                        print(f'Количество пропущенных значений: {target_missing[column]}')
-
-                        self.dataset = self.dataset.dropna(subset=[column])
-                else:
-                    print(f'\nВ колонке(ах) {self.target} нет пропущенных значений')
-
-        missing_values = self.dataset.isna().sum()
-        missing_columns = missing_values[missing_values > 0]
-        print('-------------------------------------------')
-
+    def _impute_missing_values(self, X_train, X_test):
+        missing_columns = X_train.isna().sum()
+        missing_columns = missing_columns[missing_columns > 0]
+        
         if not missing_columns.empty:
             print('В наборе данных есть пропущенные значения:')
             print("Колонки с пропущенными значениями:")
             print(missing_columns)
 
             for col in missing_columns.index:
-                if self.dataset[col].dtype in ['object','category']:
+                if X_train[col].dtype in ['object', 'category']:
                     imputer = SimpleImputer(strategy='most_frequent')
                 else:
                     imputer = SimpleImputer(strategy='median')
+                
+                X_train[col] = imputer.fit_transform(X_train[[col]]).ravel()
+                X_test[col] = imputer.transform(X_test[[col]]).ravel()
 
-                self.dataset[col] = imputer.fit_transform(self.dataset[[col]]).ravel()
-
-            missing_values_after = self.dataset.isna().sum()
-            missing_columns_after = missing_values_after[missing_values_after > 0]
-            if not missing_columns_after.empty:
-                print('После заполнения все еще есть пропущенные значения:')
-                print(missing_columns_after)
+            missing_values_after = X_train.isna().sum()
+            missing_values_after = missing_values_after[missing_values_after > 0]
+            if not missing_values_after.empty:
+                print('После заполнения все еще есть пропущенные значения в обучающем наборе:')
+                print(missing_values_after)
             else:
-                print('Все пропущенные значения заполнены.')
-
+                print('Все пропущенные значения в обучающем наборе заполнены.')
             
         else:
             print('В наборе данных нет пропущенных значений')
 
         print('-------------------------------------------')
         print('Информация о колонках в датасете')
-        categorical_cols = self.dataset.drop(self.target,axis=1).select_dtypes(include=['object', 'category']).columns
-        numeric_cols = self.dataset.select_dtypes(include=['number']).columns
+        categorical_cols = X_train.select_dtypes(include=['object', 'category']).columns
+        numeric_cols = X_train.select_dtypes(include=['number']).columns
         
         print("\nКатегориальные колонки:")
         print(categorical_cols)
@@ -95,44 +64,36 @@ class BaseDatasetTransform:
         print("\nЧисловые колонки:")
         print(numeric_cols)
 
-        if self.dataset_test is not None:
-          return self.dataset, categorical_cols, self.dataset_test
+        return X_train, X_test, categorical_cols.to_list()
 
-        return self.dataset, categorical_cols.to_list()
+    def fit_transform(self,test_size=0.3,stratify=None):
+        self.dataset = self._remove_id_columns(self.dataset)
+        
+        if self.target:
+            target_missing = self.dataset[self.target].isna().sum(axis=0)
+            target_missing_columns = target_missing[target_missing > 0]
+            print(target_missing_columns)
+            if not target_missing_columns.empty:
+                for column in target_missing_columns:
+                    print(f'\nВ колонке {column} есть пропущенные значения:')
+                    print(f'Количество пропущенных значений: {target_missing[column]}')
+                    self.dataset = self.dataset.dropna(subset=[column])
+            else:
+                print(f'\nВ колонке(ах) {self.target} нет пропущенных значений')
 
-    def get_train_test_split(self, test_size=0.3, random_state=42):
-        print('Количество значений целевой переменной по категориям:')
+
+        X = self.dataset.drop(columns=self.target)
+        y = self.dataset[self.target]
+        X_train, X_test, y_train, y_test = train_test_split(
+            X, y, test_size=test_size, random_state=42, stratify=y if stratify else None
+        )
         
 
-        if len(self.target) == 1:
+        X_train, X_test, categorical_cols = self._impute_missing_values(X_train, X_test)
 
-            y = self.dataset[self.target[0]]
-            X = self.dataset.drop(columns=self.target)
-            
-            print(self.dataset[self.target[0]].value_counts())
-            print('Следует ли выполнить стратифицированное раздеение на обучающую и тестовую выборку? y/n')
-            stratify_split = input() == 'y'
-            
-            X_train, X_test, y_train, y_test = train_test_split(
-                X, y, test_size=test_size, random_state=random_state, stratify=y if stratify_split else None
-            )
-            
-        else:
+        if self.dataset_test is not None:
+            self.dataset_test = self._remove_id_columns(self.dataset_test)
+            X_test = self._impute_missing_values(X_train, self.dataset_test)[1]
+            return X_train, X_test, y_train, y_test, self.dataset_test
 
-            y = self.dataset[self.target]
-            X = self.dataset.drop(columns=self.target)
-            
-
-            y_stratify = y.apply(lambda row: '_'.join(row.astype(str)), axis=1)
-            
-            print('Количество значений комбинаций целевых переменных по категориям:')
-            print(y_stratify.value_counts())
-            print('Следует ли выполнить стратифицированное раздеение на обучающую и тестовую выборку? y/n')
-            stratify_split = input() == 'y'
-            
-            X_train, X_test, y_train, y_test = train_test_split(
-                X, y, test_size=test_size, random_state=random_state, stratify=y_stratify if stratify_split else None
-            )
-
-        print('Разделение датасета выполнено успешно')
-        return X_train, X_test, y_train, y_test
+        return X_train, X_test, y_train, y_test,categorical_cols
